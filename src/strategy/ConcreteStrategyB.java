18921813fe12cc97205083b6b3d100a3c8fc8399
ncl <test@ncl.com>
2018-07-17 11:55:54 +0800

    策略模式

--- src/strategy/ConcreteStrategyB.java
+++ src/strategy/ConcreteStrategyB.java
@@ -3,6 +3,6 @@ package strategy;
 public class ConcreteStrategyB implements Strategy {
     @Override
     public void algorithm() {
-        System.out.println("策略B");
+        System.out.println("策略 B");
     }
 }
